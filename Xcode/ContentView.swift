0e45585c6647831501e13af6e106e7e466b6c024
Marek Melnycok <marek.melnycok@gmail.com>
2022-01-12 20:47:36 +0100

    Initial Commit

---
+++ Xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode
+//
+//  Created by Marek Melnyčok on 12.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode/XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeApp.swift
+//  Xcode
+//
+//  Created by Marek Melnyčok on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
